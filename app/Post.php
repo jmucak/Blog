63004e5b20e1124fad43e53eb6d00c1b61b526a5
jmucak <jmucak@outlook.com>
2019-03-24 14:55:05 +0100

    softDeletes

--- app/Post.php
+++ app/Post.php
@@ -3,12 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
 
     // Add title to the fillable array in your model Post, to allow saving through create and massive methods
     protected $fillable = ['title', 'content', 'category_id', 'featured'];
+
+    protected $dates = ['deleted_at'];
     // relacija sa kategorijom
     // post može imati samo jednu kategoriju
 
