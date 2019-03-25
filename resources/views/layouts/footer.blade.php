f46999088c25756e067c51d28e1b03d1cc9fb498
jmucak <jmucak@outlook.com>
2019-03-25 14:53:19 +0100

    nav, footer, app

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,2 @@
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
