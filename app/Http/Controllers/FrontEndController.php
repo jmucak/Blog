0bac3068cd2c2718bf7408678fe26dea815b15f1
jmucak <jmucak@outlook.com>
2019-03-25 16:55:24 +0100

    dynamic content display

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Setting;
+use App\Category;
+use App\Post;
+
+class FrontEndController extends Controller
+{
+    public function index() {
+
+        $page_title = Setting::first()->site_name;
+        $settings = Setting::first();
+        $categories = Category::take(5)->get(); // take(4) znači prva 4 retka, get() zbog buildera
+        $first_post = Post::orderBy('created_at', 'desc')->first();
+        $second_post = Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get()->first();
+        $third_post = Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first();
+        $laravel = Category::find(1);
+        $laravel_posts = $laravel->posts()->orderBy('created_at', 'desc')->take(3)->get();
+        $wordpress = Category::find(2);
+        $wordpress_posts = $wordpress->posts()->orderBy('created_at', 'desc')->take(3)->get();
+
+        return view('index')->with('page_title', $page_title)->with('categories', $categories)->with('first_post', $first_post)->with('second_post', $second_post)->with('third_post', $third_post)->with('laravel', $laravel)->with('wordpress', $wordpress)->with('laravel_posts', $laravel_posts)->with('wordpress_posts', $wordpress_posts)->with('settings', $settings);
+    }
+}
